1a3c72bea8810562f7f391e140320c05650063ea
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-05-27 15:01:22 +0200

    Added deobElements() and made other changes.

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,5 +2,6 @@ interface ScurInterface {
     s: string
     ob(data: string): string
     deob(data: string): string
+    deobElements(selector: string='.scur'): void
     _r(data: string): string
 }
